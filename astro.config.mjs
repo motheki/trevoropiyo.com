300e970146e1173c4321cd6b076751a292452363
Trevor Opiyo <trevoropiyo@trevoropiyo.com>
2023-01-06 17:46:41 -0600

    partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 import cloudflare from "@astrojs/cloudflare";
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({output : "server", adapter : cloudflare()});
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: cloudflare(),
+  integrations: [partytown()]
+});
