bddc44b163cde6425058c80344b85b058fd88488
Trevor Opiyo <trevoropiyo@trevoropiyo.com>
2023-01-17 19:34:27 -0600

    formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-import cloudflare from "@astrojs/cloudflare";
+import { cloudflare } from "@astrojs/cloudflare";
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-import partytown from "@astrojs/partytown";
+import { partytown } from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
