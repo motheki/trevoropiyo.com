dc08ffe22209f6090af2481d2becb47ee8629d6a
Trevor Opiyo <trevoropiyo@trevoropiyo.com>
2023-01-24 12:03:03 -0600

    prefetching

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import cloudflare from "@astrojs/cloudflare";
-import { defineConfig } from 'astro/config';
+import{defineConfig} from 'astro/config';
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
-export default defineConfig({
-  output: "server",
-  adapter: cloudflare(),
-  integrations: [partytown()]
-});
+export default defineConfig(
+    {output : "server", adapter : cloudflare(), integrations : [partytown()]});
