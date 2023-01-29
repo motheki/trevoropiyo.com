7028ec45d0b140a8b1b9cc81dcff32632490fd09
Trevor Opiyo <trevoropiyo@trevoropiyo.com>
2023-01-29 03:55:44 -0600

    optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,20 @@ import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
 import partytown from "@astrojs/partytown";
 import prefetch from "@astrojs/prefetch";
+import compress from "astro-compress";
 
+// https://astro.build/config
+import critters from "astro-critters";
+
+// https://astro.build/config
+import htmlBeautifier from "astro-html-beautifier";
+
+// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [partytown(), prefetch()]
+  integrations: [partytown(), prefetch(), compress(), critters(), htmlBeautifier()]
 });
+{
+  site: 'https://trevoropiyo.com';
+}
