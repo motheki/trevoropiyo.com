693a0c83716f321f9ab4bd9936f6d29476976b59
Trevor Opiyo <trevoropiyo@trevoropiyo.com>
2023-02-14 18:52:03 -0600

    clean upd astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
 import prefetch from "@astrojs/prefetch";
-
-// https://astro.build/config
 import critters from "astro-critters";
-
-// https://astro.build/config
 import htmlBeautifier from "astro-html-beautifier";
 
-// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
