b721d1cec2a189f078825faec65074c1305786ab
Trevor Opiyo <trevoropiyo@trevoropiyo.com>
2023-05-31 15:38:07 -0500

    file compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
 import prefetch from "@astrojs/prefetch";
+import compress from "astro-compress";
 import critters from "astro-critters";
 import htmlBeautifier from "astro-html-beautifier";
 
+// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [prefetch(), critters(), htmlBeautifier()],
+  integrations: [prefetch(), critters(), htmlBeautifier(), compress()]
 });
 {
   site: 'https://trevoropiyo.com/';
