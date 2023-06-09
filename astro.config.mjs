fa84d35e2b2c7b04e43a700551afe7e8cb1ac7ee
Trevor Opiyo <trevoropiyo@trevoropiyo.com>
2023-06-09 15:10:28 -0500

    formatting via css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import prefetch from "@astrojs/prefetch";
-import compress from "astro-compress";
 import critters from "astro-critters";
 import htmlBeautifier from "astro-html-beautifier";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [prefetch(), critters(), htmlBeautifier(), compress()]
+  integrations: [prefetch(), critters(), htmlBeautifier()]
 });
 {
   site: 'https://trevoropiyo.com/';
