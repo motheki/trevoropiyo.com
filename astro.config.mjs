a8cdb84fb11f596353698d96d86df5c47c0c008b
Trevor Opiyo <trevoropiyo@trevoropiyo.com>
2023-10-13 03:17:40 -0500

    fixed sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+   service: passthroughImageService(),
+  },
   integrations: [prefetch()]
 });
 {
