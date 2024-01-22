baa074c7a7fbaf543a8dd35d040c59b671ff1492
Trevor Opiyo <trevoropiyo@trevoropiyo.com>
2024-01-22 16:28:12 -0600

    fix the sstyling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
-import prefetch from "@astrojs/prefetch";
 
-// https://astro.build/config
 export default defineConfig({
   image: {
    service: passthroughImageService(),
   },
-  integrations: [prefetch()]
+  prefetch: {
+    prefetchAll: true
+  }
 });
+
 {
   site: 'https://trevoropiyo.com';
 }
