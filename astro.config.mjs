3d20c142a77c1c46e1a45900d18309e4927a4f7e
motheki <motheki@icloud.com>
2024-08-08 01:35:43 -0500

    starting from a new template.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,39 @@
-import { defineConfig, passthroughImageService } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import preact from "@astrojs/preact";
+import remarkToc from "remark-toc";
+import remarkCollapse from "remark-collapse";
+import sitemap from "@astrojs/sitemap";
+import { SITE } from "./src/config";
 
 export default defineConfig({
-  image: {
-   service: passthroughImageService(),
+  site: SITE.website,
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    preact({ compat: true }),
+    sitemap(),
+  ],
+  markdown: {
+    remarkPlugins: [
+      remarkToc,
+      [
+        remarkCollapse,
+        {
+          test: "Table of contents",
+        },
+      ],
+    ],
+    shikiConfig: {
+      themes: { light: "material-theme-lighter", dark: "ayu-dark" },
+      wrap: true,
+    },
   },
-  prefetch: {
-    prefetchAll: true
-  }
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
+  scopedStyleStrategy: "where",
 });
-
-{
-  site: 'https://trevoropiyo.com';
-}
