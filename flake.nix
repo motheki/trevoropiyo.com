fe13171b2a16c92033a24453707f7384f4436691
motheki <motheki@icloud.com>
2024-12-04 23:05:33 -0600

    wrangler

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
   in {
     devShells = forEachSupportedSystem ({pkgs}: {
       default = pkgs.mkShell {
-        packages = with pkgs; [alejandra bun];
+        packages = with pkgs; [alejandra bun wrangler];
       };
     });
   };
