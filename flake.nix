8abd991c007fadcdeb47fb29501e7573dff74ecd
trevoropiyo <trevoropiyo@trevoropiyo.com>
2025-10-31 18:06:34 -0500

    updates

--- flake.nix
+++ flake.nix
@@ -19,14 +19,7 @@
         inputs.treefmt-nix.flakeModule
       ];
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
-      perSystem = {
-        config,
-        self',
-        inputs',
-        pkgs,
-        system,
-        ...
-      }: {
+      perSystem = _: {
         devenv = {
           shells = {
             default = {
