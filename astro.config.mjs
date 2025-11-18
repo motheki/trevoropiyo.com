a03d40787bd387bdc4e8830eb1f06e42711cde48
trevoropiyo <trevoropiyo@trevoropiyo.com>
2025-11-18 14:15:45 -0600

    Update package dependencies, add theme support, and enhance styling across components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,33 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import fontGoogle from "@astrojs/font-google";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-	site: "https://trevoropiyo.com",
-	integrations: [mdx(), sitemap()],
-	prefetch: {
-		prefetchAll: true,
-	},
-	image: {
-		// Example: Enable the Sharp-based image service with a custom config
-		service: {
-			entrypoint: "astro/assets/services/sharp",
-			config: {
-				limitInputPixels: false,
-			},
-		},
-		experimentalLayout: "constrained",
-	},
+  site: "https://trevoropiyo.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    fontGoogle({
+      families: {
+        Quantico: true,
+      },
+    }),
+  ],
+  prefetch: {
+    prefetchAll: true,
+  },
+  experimental: {
+    clientPrerender: true,
+  },
+  image: {
+    // Example: Enable the Sharp-based image service with a custom config
+    service: {
+      entrypoint: "astro/assets/services/sharp",
+      config: {
+        limitInputPixels: false,
+      },
+    },
+    experimentalLayout: "constrained",
+  },
 });
