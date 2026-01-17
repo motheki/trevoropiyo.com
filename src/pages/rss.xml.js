57eaf432c1c005c1d69285ec6c7fc9aa7ca0b231
Trevor Opiyo <trevoropiyo@trevoropiyo.com>
2026-01-17 03:00:14 -0600

    Style fixes

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,5 @@
 import { getCollection } from "astro:content";
-import rss from "@astrojs/rss";
+import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 import MarkdownIt from "markdown-it";
 import sanitizeHtml from "sanitize-html";
 
@@ -11,13 +11,8 @@ export async function GET(context) {
     title: "Trevor Opiyo",
     description: "Trevor Opiyo's Blog",
     site: context.site,
-    items: blog.map((post) => ({
-      link: `/blog/${post.id}/`,
-      trailingSlash: false,
-      content: sanitizeHtml(parser.render(post.body), {
-        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
-      }),
-      ...post.data,
-    })),
+    items: await pagesGlobToRssItems(
+      import.meta.glob('./blog/*.{md,mdx}'),
+    ),
   });
 }
