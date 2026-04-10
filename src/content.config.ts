2b38f71a2ffbe35ea6b36037e948f04daa9052f0
Trevor Opiyo <trevoropiyo@trevoropiyo.com>
2026-04-10 01:56:55 -0500

    Finishing polish

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,16 +1,16 @@
-import { defineCollection} from "astro:content";
+import { defineCollection } from "astro:content";
 import { z } from "astro/zod";
 import { glob } from "astro/loaders";
 import { rssSchema } from "@astrojs/rss";
 
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog" }),
-  schema: rssSchema.extend({
+  schema: ({ image }) => rssSchema.extend({
     title: z.string(),
     description: z.string(),
     pubDate: z.union([z.string(), z.number(), z.date()]).transform((val) => new Date(val)),
     updatedDate: z.union([z.string(), z.number(), z.date()]).transform((val) => new Date(val)).optional(),
-    heroImage: z.string().optional(),
+    heroImage: image().optional(),
   }),
 });
 
