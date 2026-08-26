bc0abc8182d2204c19339cecca1925654f510f04
Trevor Opiyo <trevoropiyo@trevoropiyo.com>
2026-08-25 22:59:25 -0500

    Formatting

--- flake.nix
+++ flake.nix
@@ -3,22 +3,25 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/master";
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
     };
     flake-parts = {
       url = "github:hercules-ci/flake-parts/main";
     };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     devenv = {
       url = "github:cachix/devenv/main";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs =
-    inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
       imports = [
         inputs.devenv.flakeModule
+        inputs.treefmt-nix.flakeModule
       ];
       systems = [
         "x86_64-linux"
@@ -26,26 +29,42 @@
         "aarch64-darwin"
         "x86_64-darwin"
       ];
-      perSystem =
-        { ... }:
-        {
-          devenv.shells.default = {
-            name = "trevoropiyo.com";
-            languages = {
-              javascript = {
+      perSystem = {...}: {
+        treefmt = {
+          enableDefaultExcludes = true;
+          programs = {
+            alejandra.enable = true;
+            deadnix.enable = true;
+            mdformat.enable = true;
+            oxfmt.enable = true;
+            taplo.enable = true;
+          };
+        };
+
+        devenv.shells.default = {
+          name = "trevoropiyo.com";
+          cachix.enable = true;
+          languages = {
+            nix = {
+              enable = true;
+              lsp = {
                 enable = true;
-                lsp = {
+              };
+            };
+            javascript = {
+              enable = true;
+              lsp = {
+                enable = true;
+              };
+              bun = {
+                enable = true;
+                install = {
                   enable = true;
-                };
-                bun = {
-                  enable = true;
-                  install = {
-                    enable = true;
-                  };
                 };
               };
             };
           };
         };
+      };
     };
 }
